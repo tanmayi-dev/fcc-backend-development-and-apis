1e7107535a942ac1ff6aa3cae500c73a76fe82c4
Tanmayi D <60173146+tanmayidev@users.noreply.github.com>
2024-09-16 00:09:12 +0530

    task: [MDB-11] Delete Many Documents with model.remove()

--- boilerplate-mongomongoose/myApp.js
+++ boilerplate-mongomongoose/myApp.js
@@ -90,8 +90,10 @@ const removeById = (personId, done) => {
 
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
-
-  done(null /*, data*/);
+  Person.remove({ name: nameToRemove }, (err, response) => {
+    if (err) return console.log(err);
+    done(null, response);
+  });
 };
 
 const queryChain = (done) => {
