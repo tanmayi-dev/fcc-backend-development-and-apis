be87a78d68adb47daac6b4289915e86c6c793a64
Tanmayi D <60173146+tanmayidev@users.noreply.github.com>
2024-09-15 17:38:32 +0530

    task: [EXP-03] serve html file

--- boilerplate-express/myApp.js
+++ boilerplate-express/myApp.js
@@ -1,6 +1,13 @@
 let express = require("express");
 let app = express();
 
+// Challenge 3
+const filePath = __dirname + "/views/index.html";
+
+app.get("/", (req, res) => {
+  res.sendFile(filePath);
+});
+
 // Challenge 2
 app.get("/", (req, res) => {
   res.send("Hello Express");
