00990d6e22bd3e8cfd032b24b1cb65bf011cad3a
Tanmayi D <60173146+tanmayidev@users.noreply.github.com>
2024-09-15 17:38:02 +0530

    task: [EXP-02] start server and print Hello Express

--- boilerplate-express/myApp.js
+++ boilerplate-express/myApp.js
@@ -1,6 +1,11 @@
 let express = require("express");
 let app = express();
 
+// Challenge 2
+app.get("/", (req, res) => {
+  res.send("Hello Express");
+});
+
 // Challenge 1
 console.log("Hello World");
 
