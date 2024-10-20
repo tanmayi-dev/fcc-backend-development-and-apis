5382c5c7d468c2cc3197144737aedaac5a38dd01
Tanmayi D <60173146+tanmayidev@users.noreply.github.com>
2024-10-20 09:52:20 +0530

    PROJ: [P2-02] add whoami api

--- projects/02-request-header-parser-microservice/index.js
+++ projects/02-request-header-parser-microservice/index.js
@@ -19,6 +19,20 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
+// API route to return header info
+app.get("/api/whoami", (req, res) => {
+  const ipaddress =
+    req.ip || req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+  const language = req.headers["accept-language"];
+  const software = req.headers["user-agent"];
+
+  res.json({
+    ipaddress,
+    language,
+    software,
+  });
+});
+
 // your first API endpoint...
 app.get("/api/hello", function (req, res) {
   res.json({ greeting: "hello API" });
